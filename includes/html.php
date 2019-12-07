7ec04858a4b1edcddd20cbefb8b6d53aab69e838
Arthur CLARKE <arthurclarke312@gmail.com>
2019-12-07 17:05:47 +0100

    correction

--- includes/html.php
+++ includes/html.php
@@ -1,8 +1,10 @@
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
+
   <head>
-     <link rel="stylesheet" href="./assets/css/styles.css">
     <meta charset="utf-8">
+    <title>Sondages</title>
+     <link rel="stylesheet" href="./assets/css/styles.css">
     <title>Sondages</title>
   </head>
   <body>
