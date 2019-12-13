decb51d48e479cee44fa12d14bf32ea8c4c4003f
Arthur CLARKE <arthurclarke312@gmail.com>
2019-12-13 12:19:55 +0100

    sasa

--- includes/header.php
+++ includes/header.php
@@ -6,7 +6,7 @@
     <li><a href ="index.php?page=sondage" title="Répondez au sondage de le semaine">Sondage</a></li>
     <li><a href ="index.php?page=contact" title="Nous contacter">Contact</a></li>
     <li><a href ="index.php?page=mentions" title="Les mentions légales de le site">Mentions légales</a></li>
-    <li><a href ="index.php?page=inscription" title="Viens t'inscrire">Inscription</a></li>
+    <li><a href ="index.php?page=inscription" title="Viens t'inscrire">Inscrivation</a></li>
   </ul>
 </nav>
 
